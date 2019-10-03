f5e4cb055d2333405b89e0e1c722bcbd0b3f678d
Kaulitz Guimaraes <kaulitz.guimaraes@ibm.com>
2019-10-03 15:09:18 -0300

    adding console log

--- PraticalExercises/DollarPrice/index.js
+++ PraticalExercises/DollarPrice/index.js
@@ -3,7 +3,7 @@
 fetch('https://api.exchangeratesapi.io/latest?symbols=USD,BRL')
   .then(response => response.json())
   .then(json => {
-    
+    console.log(json)
     document.querySelector("#real").textContent = json.rates.BRL
 document.querySelector("#dolar").textContent = json.rates.USD
   })
